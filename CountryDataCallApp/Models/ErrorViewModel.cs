80ddffa0f6aba38f1dbda1f4387fec4f7b503323
Adam <audovich@umich.edu>
2018-08-04 20:54:11 -0400

    Initial

---
+++ CountryDataCallApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CountryDataCallApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
